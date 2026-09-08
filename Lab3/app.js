b8e5352d082b905572ecfafed92732f5cf15ed95
ARYANCSE15 <aryansharma0379@gmail.com>
2026-09-08 09:39:41 +0530

    8/09/26

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,6 +1,7 @@
 import http from "http";
+import team from "./teams";
 const server=http.createServer((req,res)=>{
-    res.end("<h2>Welcome to the server</h2>");
+    res.end("<h2>SIH INTERNAL</h2>");
 
 });
 server.listen(5000,()=>{
