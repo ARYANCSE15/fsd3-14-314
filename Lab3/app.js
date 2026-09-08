85775ccce573a145846880141fb962e65bf9e8ad
ARYANCSE15 <aryansharma0379@gmail.com>
2026-09-08 22:29:09 +0530

    8/09/26

--- Lab3/app.js
+++ Lab3/app.js
@@ -24,6 +24,18 @@ const server=http.createServer((req,res)=>{
     const{pathname,query}=parseUrl(req.url,true);
     console.log(pathname);
     console.log(query);
+    console.log("pathname:",pathname);
+    console.log("query:",query);
+    console.log("method:",method);
+    if(pathname==="/api/v1/teams"&& method==="GET"){
+        let teams=getALLTeams();
+        return sendJson(res,200,teams);
+    }
+    else{
+        res.statusCode=404;
+    }
+    res.end();
+
 });
 server.listen(PORT,()=>{
     console.log("SIH Server is running at",PORT);
